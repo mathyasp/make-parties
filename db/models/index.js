4ccb00edef0c05809f6f0a4b0f555ec2404210a4
mathyasp <mathyastp@gmail.com>
2024-04-01 19:59:15 -0400

    deployment updates

--- db/models/index.js
+++ db/models/index.js
@@ -12,6 +12,7 @@ const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  console.log('DATABASE_URL:', process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
