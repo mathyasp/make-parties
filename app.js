5a8d7fe727bd7664f6db53d9ec4eccd1ca91d4b9
mathyasp <mathyastp@gmail.com>
2024-04-01 19:23:43 -0400

    Users can see single events

--- app.js
+++ app.js
@@ -48,10 +48,19 @@ app.get('/events/new', (req, res) => {
 // CREATE
 app.post('/events', (req, res) => {
   models.Event.create(req.body).then(event => {
-    res.redirect(`/`);
+    res.redirect(`/events/${event.id}`);
   }).catch((err) => {
     console.log(err)
   });
+})
+
+// SHOW
+app.get('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-show', { event: event })
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 // Choose a port to listen on
