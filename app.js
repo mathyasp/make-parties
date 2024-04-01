c51b7c9c82e08630164421fab4050e407612fdec
mathyasp <mathyastp@gmail.com>
2024-04-01 16:27:32 -0400

    Users can create events

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Initialize express
-const express = require('express')
-const app = express()
-
-// require handlebars
+const express = require('express');
+const app = express();
 const { engine } = require('express-handlebars');
-const Handlebars = require('handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const Handlebars = require('handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const bodyParser = require('body-parser');
+const models = require('./db/models');
 
 // Use "main" as our default layout
 app.engine('handlebars', engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
-// Use handlebars to render
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'handlebars');
 
 // OUR MOCK ARRAY OF PROJECTS
@@ -30,9 +30,30 @@ var events = [
     imgUrl: "https://i.pinimg.com/736x/8f/dc/25/8fdc2575e44431c6c08b4069efd42e75.jpg" }
 ]
 
-// INDEX
+// Index
 app.get('/', (req, res) => {
-  res.render('events-index', { events: events });
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+    res.render('events-index', { events: events });
+  })
+})
+
+// NEW
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+});
+
+// CREATE
+app.post('/events', (req, res) => {
+  console.log(req.body);
+});
+
+// CREATE
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
 })
 
 // Choose a port to listen on
