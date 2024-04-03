5418245896fe9b10d93a3e715af78ca0ee576444
mathyasp <mathyastp@gmail.com>
2024-04-03 12:28:38 -0400

    Added createdAt, footer, and bootstrap theme

--- controllers/events.js
+++ controllers/events.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 module.exports = function (app, models) {
 
   // INDEX
@@ -24,6 +25,9 @@ module.exports = function (app, models) {
   // SHOW
   app.get('/events/:id', (req, res) => {
     models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
+      let createdAt = event.createdAt;
+      createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+      event.createdAtFormatted = createdAt;
       res.render('events-show', { event: event });
     }).catch((err) => {
       console.log(err.message);
